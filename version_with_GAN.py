992342156a0b3c4aac72c919589cb808bf8de6c3
James Lawson <jameslawson00031@gmail.com>
2021-04-14 12:28:14 -0400

    Update version_with_GAN.py

--- version_with_GAN.py
+++ version_with_GAN.py
@@ -1,6 +1,4 @@
-
-# Libraries we are currently using
-
+#imports
 import csv
 import keras
 import matplotlib.pyplot as plt
@@ -10,17 +8,6 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from keras.layers import Conv2D
 import time
-
-#Libraries we are not using currently / probably don't need
-
-# import pandas as pd
-# import copy
-# import glob
-# import imageio
-# import os
-# import PIL
-# from IPython import display
-
 
 # Make sure the optimizer variables are global
 generator_optimizer = tf.keras.optimizers.Adam(1e-4)
@@ -51,7 +38,7 @@ def get_data(file):
     # Normalize the puzzles between -1 and 1
     feat = np.array(feat)    
     feat = feat/9
-    feat -= .5    
+    feat -= .5
     
     for i in label_raw:
         x = np.array([int(j) for j in i]).reshape((81,1)) - 1
@@ -69,14 +56,14 @@ def get_data(file):
 
 def make_generator_model():
     
-    inps = layers.Input(shape=(9,9))
-    x = layers.Dense(9, activation='relu',
-        kernel_initializer='he_uniform')(inps)
+    inps = layers.Input(shape = (9,9))
+    x = layers.Dense(9, activation = 'relu',
+        kernel_initializer = 'he_uniform')(inps)
     # x = Conv2D(64, kernel_size = (3,3),
         # activation = 'relu', padding = 'same',
         # input_shape = (9,9))(inps)
-    outs = layers.Dense(9, activation='tanh')(x)
-    model = keras.Model(inps, outs, name='generator')
+    outs = layers.Dense(9, activation = 'tanh')(x)
+    model = keras.Model(inps, outs, name = 'generator')
     
     # old model
     
@@ -105,11 +92,11 @@ def make_generator_model():
 
 def make_discriminator_model():
     
-    inps = layers.Input(shape=(9,9))
-    x = layers.Dense(9, activation='relu',
-        kernel_initializer='he_uniform')(inps)
+    inps = layers.Input(shape = (9,9))
+    x = layers.Dense(9, activation = 'relu',
+        kernel_initializer = 'he_uniform')(inps)
     outs = layers.Dense(1)(x)
-    model = keras.Model(inps, outs, name='discriminator')
+    model = keras.Model(inps, outs, name = 'discriminator')
     
     # old model
     
@@ -145,19 +132,15 @@ def generator_loss(fake_output):
     
     return cross_entropy(tf.ones_like(fake_output), fake_output)
 
-# Notice the use of `tf.function`
-# This annotation causes the function to be "compiled".
-
-@tf.function
-def train_step(images, generator, discriminator,noise_dim):
+def train_step(text, generator, discriminator,noise_dim):
 ##    noise = tf.random.uniform([9, 9], 0, 9, dtype=tf.dtypes.int64)
     noise = tf.random.normal([9, noise_dim])
 
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-        generated_images = generator(noise, training=True)
+        generated_text = generator(text, training=True)
 
-        real_output = discriminator(images, training=True)
-        fake_output = discriminator(generated_images, training=True)
+        real_output = discriminator(text, training=True)
+        fake_output = discriminator(generated_text, training=True)
 
         gen_loss = generator_loss(fake_output)
         disc_loss = discriminator_loss(real_output, fake_output)
@@ -170,35 +153,44 @@ def train_step(images, generator, discriminator,noise_dim):
 
 def train(dataset, epochs, generator, discriminator, noise_dim, seed):
     for epoch in range(epochs):
-        start = time.time()
 
-        for image_batch in dataset:
-            train_step(image_batch, generator, discriminator, noise_dim)
+        for sudoku_puzzle in dataset:
+            train_step(sudoku_puzzle, generator, discriminator, noise_dim)
 
-        print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+        print("epoch #: ", epoch + 1)
 
 def main():
     
     x_train, x_test, y_train, y_test = get_data('sudoku.csv')
+
+    #we have 80,000 training examples
     BUFFER_SIZE = 800000
     BATCH_SIZE = 800000
     train_dataset = tf.data.Dataset.from_tensor_slices(x_train).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
     
     generator = make_generator_model()
     discriminator = make_discriminator_model()
-    noise = tf.random.uniform([9, 9], 0, 9, dtype=tf.dtypes.int64)
-    print(noise)
-    generated_image = generator(noise, training = False) #either
-    print(generator.summary(), discriminator.summary())
 
     EPOCHS = 50
     noise_dim = 9
     num_examples_to_generate = 16
 
-    # You will reuse this seed overtime (so it's easier)
-    # to visualize progress in the animated GIF)
     seed = tf.random.normal([num_examples_to_generate, noise_dim])
     train(train_dataset, EPOCHS, generator, discriminator, noise_dim, seed)
+
+    noise = tf.random.uniform([9, 9], 0, 9, dtype=tf.dtypes.int64)
+    print("noise: ", noise)
+    generated_puzzle = generator(noise, training = True)
+
+    print("old generated puzzle: ", generated_puzzle)
+    
+    #need to un-normalize the puzzles between -1 and 1
+    generated_puzzle = generated_puzzle * 9
+    generated_puzzle += .5
+
+    print("new generated puzzle: ", generated_puzzle)
+
+    #print(generator.summary(), discriminator.summary())
 
 if __name__ == '__main__':
     main()
